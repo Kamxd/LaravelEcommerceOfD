8c38e990f3b249a6818f75be6190233108b5a014
Karsten .D <karstenahouangan@gmail.com>
2023-01-27 15:08:41 +0100

    create main controller to show article

--- ecommerceOfD/app/Http/Controllers/MainController.php
+++ ecommerceOfD/app/Http/Controllers/MainController.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\shop;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function index(){
+
+        return view('shop/index');
+    }
 }
